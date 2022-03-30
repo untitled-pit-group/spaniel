f455cd1995a0ad7bfa801fca1c6e6325aa351cc0
paulsnar <paulsnar@paulsnar.lv>
2022-03-30 13:18:22 +0300

    feat(FX): Implement Foxhound connection wrapper widget
    This should be alright to replace GetX's approach and also provide some
    flexibility, such as regarding the changing of the client being provided
    at any one moment. (Including, potentially, dynamically!)

--- lib/pifs/fakes/fake_client.dart
+++ lib/pifs/fakes/fake_client.dart
@@ -1,17 +1,12 @@
 import "package:spaniel/pifs/client.dart";
 import "package:spaniel/pifs/data/file.dart";
 import "package:spaniel/pifs/data/upload.dart";
-import "package:spaniel/pifs/parameters/files_edit.dart";
-import "package:spaniel/pifs/parameters/uploads_begin.dart";
-import "package:spaniel/pifs/parameters/uploads_cancel.dart";
-import "package:spaniel/pifs/parameters/uploads_finish.dart";
-import "package:spaniel/pifs/responses/null_response.dart";
+import "package:spaniel/pifs/parameters/parameters.dart";
+import "package:spaniel/pifs/responses/responses.dart";
 
 class PifsFakeClient implements PifsClient {
-  static Future<PifsFakeClient> build() async {
-    await Future.delayed(const Duration(milliseconds: 500));
-    return PifsFakeClient();
-  }
+  const PifsFakeClient();
+  static const instance = PifsFakeClient();
 
   @override
   PifsResponse<PifsFile> filesEdit(PifsFilesEditParameters params) {
---
+++ lib/pifs/support/flutter.dart
@@ -0,0 +1,80 @@
+import "dart:async";
+import "package:flutter/material.dart";
+import "package:spaniel/foxhound/client.dart";
+import "package:spaniel/pifs/client.dart";
+import "package:spaniel/pifs/fakes/fake_client.dart";
+import "package:spaniel/pifs/fakes/offline_client.dart";
+
+class PifsClientProvider extends InheritedWidget {
+  final PifsClient client;
+
+  const PifsClientProvider({
+    Key? key,
+    required this.client,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(PifsClientProvider oldWidget) => oldWidget.client != client;
+
+  static PifsClient of(BuildContext context) {
+    final provider = context.dependOnInheritedWidgetOfExactType<PifsClientProvider>();
+    assert(provider != null, "No PifsClientProvider found in context");
+    return provider!.client;
+  }
+}
+
+class _PifsClientConnectorState extends State<PifsFoxhoundClientConnector> {
+  late PifsClient client;
+
+  Future<void> _connect() async {
+    final completer = Completer<PifsClient>.sync();
+    client = PifsIndeterminateClient(completer.future);
+    try {
+      final client = await FoxhoundClient.build();
+      setState(() {
+        this.client = client;
+        completer.complete(client);
+      });
+    } on Error {
+      // TODO[pn]: This should probably be a bit more conditional than this
+      // TODO[pn]: Connection should be retried on an interval *and* upon detecting any connectivity state changes
+      setState(() {
+        // TODO[pn]: [PifsOfflineReason.badSecret] should be detected
+        client = const PifsOfflineClient(PifsOfflineReason.networkConnection);
+        completer.complete(client);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _connect();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PifsClientProvider(client: client, child: widget._child);
+  }
+}
+class PifsFoxhoundClientConnector extends StatefulWidget {
+  final Widget _child;
+  const PifsFoxhoundClientConnector({
+    Key? key,
+    required Widget child,
+  }) : _child = child, super(key: key);
+
+  @override
+  State<PifsFoxhoundClientConnector> createState() => _PifsClientConnectorState();
+}
+
+class PifsFakeClientConnector extends StatelessWidget {
+  final Widget child;
+  const PifsFakeClientConnector({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PifsClientProvider(client: PifsFakeClient.instance, child: child);
+  }
+}
