9765630783b00b93a361b8087c0ab4926e078bf2
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2022-04-05 20:23:22 +0300

    chore: moved fake file generation to FakePifsClient

--- lib/pifs/fakes/fake_client.dart
+++ lib/pifs/fakes/fake_client.dart
@@ -1,3 +1,8 @@
+import "dart:convert";
+import "dart:math";
+import "package:crypto/crypto.dart";
+import "package:dartz/dartz.dart";
+import "package:list_ext/list_ext.dart";
 import "package:spaniel/pifs/client.dart";
 import "package:spaniel/pifs/data/file.dart";
 import "package:spaniel/pifs/data/upload.dart";
@@ -8,6 +13,24 @@ import "package:spaniel/pifs/parameters/uploads_finish.dart";
 import "package:spaniel/pifs/responses/null_response.dart";
 
 class PifsFakeClient implements PifsClient {
+  static const _chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+  final Random _rnd = Random();
+
+  String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+      length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+
+  PifsFile _generateFakeFile() => PifsFile(
+      id: getRandomString(16),
+      name: getRandomString(16),
+      tags: ["Iyama Tag", "Birka", "Kakas Emodži", "💩"].where((_) => _rnd.nextBool()).toList(),
+      uploadTimestamp: DateTime.now().subtract(Duration(hours: _rnd.nextInt(5000))),
+      relevanceTimestamp: _rnd.nextBool() ? DateTime.now().subtract(Duration(hours: _rnd.nextInt(5000))) : null,
+      length: _rnd.nextInt(500000),
+      hash: sha256.convert(utf8.encode(getRandomString(20))).toString(),
+      type: ["document", "plain", "media"].random,
+      indexingState: _rnd.nextInt(5)
+  );
+
   static Future<PifsFakeClient> build() async {
     await Future.delayed(const Duration(milliseconds: 500));
     return PifsFakeClient();
@@ -19,8 +42,9 @@ class PifsFakeClient implements PifsClient {
   }
 
   @override
-  PifsResponse<List<PifsFile>> filesList() {
-    throw UnimplementedError();
+  PifsResponse<List<PifsFile>> filesList() async {
+    await Future.delayed(const Duration(milliseconds: 200));
+    return Left(List.generate(20, (_) => _generateFakeFile()));
   }
 
   @override
--- lib/spaniel/screens/home.dart
+++ lib/spaniel/screens/home.dart
@@ -1,47 +1,14 @@
-import "dart:convert";
-import "dart:math";
 
 import "package:flutter/material.dart";
-import "package:flutter_bloc/flutter_bloc.dart";
-import "package:list_ext/list_ext.dart";
-import "package:spaniel/pifs/client.dart";
-import "package:spaniel/pifs/data/file.dart";
-import "package:crypto/crypto.dart";
-import "package:spaniel/spaniel/bloc/file.dart";
-import "package:spaniel/spaniel/screens/file.dart";
-import "package:spaniel/spaniel/screens/upload.dart";
 import "package:spaniel/spaniel/widgets/file_item.dart";
 
-const _chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
-Random _rnd = Random();
 
-String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
-    length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
 
 class SPHome extends StatelessWidget {
   const SPHome({Key? key}) : super(key: key);
 
-  PifsFile _generateFakeFile() => PifsFile(
-      id: getRandomString(16),
-      name: getRandomString(16),
-      tags: ["Iyama Tag", "Birka", "Kakas Emodži", "💩"],
-      uploadTimestamp: DateTime.now().subtract(Duration(hours: Random().nextInt(5000))),
-      relevanceTimestamp: Random().nextBool() ? DateTime.now().subtract(Duration(hours: Random().nextInt(5000))) : null,
-      length: Random().nextInt(500000),
-      hash: sha256.convert(utf8.encode(getRandomString(20))).toString(),
-      type: ["document", "plain", "media"].random,
-      indexingState: Random().nextInt(5)
-  );
-
   Widget _getFileList(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text("my_files", style: Theme.of(context).textTheme.headlineLarge),
-        ...List.generate(20, (_) => _generateFakeFile())
-        .map((e) => SPFileItem(file: e))
-      ]
-    );
+    return Text("ssssssshhh is ok");
   }
 
   Widget _getContents(BuildContext context) {
